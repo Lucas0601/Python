3442bbe8491407c63599f044779b07bc011492eb
Lucas Araújo <lucasba0601@gmail.com>
2022-02-17 22:52:42 -0300

    Add files via upload

---
+++ UtilizacaoDeMetodosEFuncoes.py
@@ -0,0 +1,32 @@
+# 1.Aprender a utilização de métodos e funções no python
+# 2.Aprender a utilização de classes
+# 3.Entender os motivos para se usar métodos, funções e classes
+
+class Calculadora:
+    def __init__(self, num1, num2):
+        self.valor_a = num1
+        self.valor_b = num2
+    def soma (self):
+        return self.valor_a + self.valor_b
+
+    def subtracao (self) :
+        return self.valor_a - self.valor_b
+
+    def multiplicacao(self):
+        return self.valor_a * self.valor_b
+
+    def divisao(self):
+        return self.valor_a / self.valor_b
+calculadora = Calculadora(10, 2)
+print(calculadora.valor_a)
+print(calculadora.soma())
+print(calculadora.subtracao())
+print(calculadora.divisao())
+print(calculadora.multiplicacao())
+
+
+# print (soma(1, 2))
+# print (soma(3, 4))
+# print(subtracao(10, 2))
+# print(divisao(10, 2))
+# print(multiplicacao((10, 2))
